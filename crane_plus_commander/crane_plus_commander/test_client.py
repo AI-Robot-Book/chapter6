83baf6346f79da610d44cc302a17321adb4e6208
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2022-08-23 17:18:59 +0900

    test_clientを整理

--- crane_plus_commander/crane_plus_commander/test_client.py
+++ crane_plus_commander/crane_plus_commander/test_client.py
@@ -1,44 +1,37 @@
-from airobot_interfaces.srv import StringCommand
 import rclpy
 from rclpy.node import Node
+from airobot_interfaces.srv import StringCommand
 
 
 class TestClient(Node):
 
     def __init__(self):
         super().__init__('test_client')
-        self.cli = self.create_client(StringCommand, 'command')
-        while not self.cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-        self.req = StringCommand.Request()
+        self.client = self.create_client(StringCommand, 'manipulation/command')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('サービス無効，待機中...')
 
     def send_request(self, command):
-        self.req.command = command
-        self.future = self.cli.call_async(self.req)
+        request = StringCommand.Request()
+        request.command = command
+        future = self.client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result().answer
 
 
 def main():
     rclpy.init()
 
-    test_client = TestClient()
+    node = TestClient()
 
-    while True:
-        command = input('command: ')
-        if command == '':
-            break
-        test_client.send_request(command)
-
-        while rclpy.ok():
-            rclpy.spin_once(test_client)
-            if test_client.future.done():
-                try:
-                    response = test_client.future.result()
-                except Exception as e:
-                    test_client.get_logger().info(
-                        'Service call failed %r' % (e,))
-                else:
-                    test_client.get_logger().info(
-                        f'answer: {response.answer}')
+    try:
+        while True:
+            command = input('command: ')
+            if command == '':
                 break
+            answer = node.send_request(command)
+            print(f'answer: {answer}')
+    except KeyboardInterrupt:
+        pass
 
     rclpy.shutdown()
